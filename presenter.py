71ebc162b1e20e338174e4d63930dfb4171d2f6f
Xenocider <xenos.lu@gmail.com>
2020-04-22 14:08:51 +0800

    updated

--- presenter.py
+++ presenter.py
@@ -127,8 +127,7 @@ def stop_and_rename_container(name):
         return None
     container.rename('%s_old_%s' % (name, digit_now()))
     container.stop()
-    container.update(restart_policy = {'Name': ''})
-    container.reload()
+    container.update(restart_policy = {'Name': 'no'})
     return container
 
 
