eea5a363f26d8c8ca2addcd1a5aba978a184d2e8
Xenocider <xenos.lu@gmail.com>
2020-06-01 10:14:09 +0800

    perf

--- cap.py
+++ cap.py
@@ -38,6 +38,7 @@ def authenticated_async(login=LOGIN):
             res = await oauth.get_user_with_cache(token)
             if login and not res.get('login') == login:
                 logging.info('unauthorized request user: %s' % res.get('login'))
+                self.clear_cookie('github_access_token')
                 return
             self.current_user = res.get('login')
             logging.info('user login: %s' % self.current_user)
